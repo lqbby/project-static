91cf2e260a47229a661a178d9525236fd648fd99
lqbby2 <1151581339@qq.com>
2022-10-16 09:01:37 +0800

    提交

--- js/common.js
+++ js/common.js
@@ -6,60 +6,72 @@ document.addEventListener('DOMContentLoaded', () => {
   }, 200)
 })
 
+
 // 每个页面都会导入该js 统一的设置写这里即可
+/**
+ * 作用：在当前页面出现一个提示消息 过一会自己就会消失
+ * 用户体验更好的alert 
+ * @param {提示的文本} msg 
+ */
+
 function tip(msg) {
-  const myToast = new bootstrap.Toast(document.querySelector('#myToast'), {
-    delay: 1000,
+  // new bootstrap.Toast(toastEl, option) 
+  const myToast = new bootstrap.Toast(document.querySelector("#myToast"), {
+    delay:1000
   })
-  document.querySelector('#myToast').innerHTML = msg
-  myToast.show()
+  document.querySelector("#myToast .toast-body").innerHTML = msg
+  myToast.show()//显示
 }
-// 配置xiaos基德值
+
+// 配置ajax基地址
 axios.defaults.baseURL = 'http://ajax-api.itheima.net'
-const usernameSelector = document.querySelector('.mb-0.font-weight-bold')
-const usernameSelector1 = document.querySelector('.mb-0.text-white')
-if (usernameSelector) {
+
+
+// 把用户名字从本地存储拿出来,然后渲染到页面中(index.html,student.html) 
+const usernameSelector = document.querySelector(".mb-0.font-weight-bold")
+// console.log(usernameSelector);
+if (usernameSelector) {//有二个页面是没有这个东西,register.html,login.html 
   usernameSelector.innerHTML = localStorage.getItem('username')
-  usernameSelector1.innerHTML = localStorage.getItem('username')
 }
-const logout = document.querySelector('#logout')
-if (logout) {
-  logout.onclick = function () {
-    localStorage.removeItem('token')
+
+
+// 退出功能 因为有二个页面有这个功能
+const logoutBtn = document.querySelector('#logout')
+if (logoutBtn) {
+  logoutBtn.onclick = function () { 
     localStorage.removeItem('username')
+    localStorage.removeItem('token')
     location.href = './login.html'
   }
 }
 
+
 // 添加请求拦截器
-axios.interceptors.request.use(
-  function (config) {
-    config.headers.Authorization = localStorage.getItem('token')
-    // 在发送请求之前做些什么
-    return config
-  },
-  function (error) {
-    // 对请求错误做些什么
-    return Promise.reject(error)
-  }
-)
+axios.interceptors.request.use(function (config) {
+  config.headers.Authorization = localStorage.getItem('token')
+  // 在发送请求之前做些什么
+  return config;//什么也没有做 原样返回
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
 
 // 添加响应拦截器
-axios.interceptors.response.use(
-  function (response) {
-    // 2xx 范围内的状态码都会触发该函数。
-    // 对响应数据做点什么
-    return response.data
-  },
-  function (error) {
-    console.dir(error)
-    logout.onclick = function () {
-      localStorage.removeItem('token')
-      localStorage.removeItem('username')
-      location.href = './login.html'
-    }
-    // 超出 2xx 范围的状态码都会触发该函数。
-    // 对响应错误做点什么
-    return Promise.reject(error)
+axios.interceptors.response.use(function (response) {
+  // console.log(response);
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据做点什么
+  return response.data 
+}, function (error) {
+  console.dir(error)
+  //防止token过了有效期 直接让用户重新登录
+  if (error.response.status === 401) {
+    //说明token是过期了
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    location.href = './login.html'
   }
-)
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
--- js/index.js
+++ js/index.js
@@ -1,1 +1,249 @@
-document.querySelector('.text-white').innerHTML = localStorage.getItem('username')
+window.onload = async function () {
+    const res = await axios({ url: '/dashboard' })
+    console.log(res);
+    const { year, salaryData, groupData } = res.data
+    // console.log(year);
+    // 设置标题部分
+    for (let key in res.data.overview) {
+        console.log(`[name="${key}"]`);
+        document.querySelector(`[name="${key}"]`).innerHTML = res.data.overview[key];
+    }
+
+    // 全学科薪资走势
+    renderSubject(year)
+    //班级薪资
+    renderClass(salaryData)
+    // 班级每组薪资
+    renderClassSalary(groupData)
+}
+
+
+function renderSubject(arr) {
+    var myChart = echarts.init(document.getElementById('line'));
+
+    // 指定图表的配置项和数据
+    var option = {
+        title: {
+            text: '2022全学科薪资走势',
+            top: '10',
+            left: '10'
+        },
+        xAxis: {
+            type: 'category',
+            data: arr.map(item => item.month),
+            axisLine: {
+                lineStyle: {
+                    type: 'dashed',
+                    color: 'skyblue'
+                }
+            },
+            axisLabel: {
+                color: '#000'
+            }
+        },
+        yAxis: {
+            type: 'value',
+            splitLine: {
+                lineStyle: {
+                    type: 'dashed',
+                    color: 'skyblue'
+                }
+            }
+        },
+        series: [
+            {
+                data: arr.map(item => item.salary),
+                type: 'line',
+                smooth: true,
+                lineStyle: {
+                    width: 8,
+                    color: '#5f8eff'
+                },
+                symbolSize: 10,
+                areaStyle: {
+                    color: {
+                        type: 'linear',
+                        x: 0,
+                        y: 0,
+                        x2: 0,
+                        y2: 1,
+                        colorStops: [{
+                            offset: 0, color: '#c8e2ff' // 0% 处的颜色
+                        }, {
+                            offset: 0.8, color: '#fff' // 100% 处的颜色
+                        }],
+                        global: false // 缺省为 false
+                    }
+                }
+            }
+        ]
+    };
+
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+
+}
+
+function renderClass(arr) {
+    var myChart = echarts.init(document.getElementById('salary'));
+
+    // 指定图表的配置项和数据
+    var option = {
+        title: {
+            text: '班级薪资分布'
+        },
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            bottom: '5%',
+            left: 'center'
+        },
+        series: [
+            {
+                name: 'Access From',
+                type: 'pie',
+                radius: ['40%', '50%'],
+                avoidLabelOverlap: false,
+                itemStyle: {
+                    borderRadius: 10,
+                    borderColor: '#fff',
+                    borderWidth: 2
+                },
+                label: {
+                    show: false,
+                    position: 'center'
+                },
+                emphasis: {
+                    label: {
+                        show: true,
+                        fontSize: '40',
+                        fontWeight: 'bold'
+                    }
+                },
+                labelLine: {
+                    show: false
+                },
+                data: arr.map(item => ({
+                    value: item.g_count + item.b_count,
+                    name: item.label
+                }))
+            }
+        ],
+        color: ['red', 'blue', 'pink', 'green', 'yellow']
+    };
+
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+}
+
+
+function renderClassSalary(arr) {
+    var myChart = echarts.init(document.getElementById('lines'));
+
+    // 指定图表的配置项和数据
+    var option = {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                data: [120, 200, 150, 80, 70, 110, 130],
+                type: 'bar'
+            },
+            {
+                data: [120, 200, 150, 80, 70, 110, 130],
+                type: 'bar'
+            }
+        ],
+        color: [
+            {
+                type: 'linear',
+                x: 0,
+                y: 0,
+                x2: 0,
+                y2: 1,
+                colorStops: [
+                    {
+                        offset: 0,
+                        color: '#3ad39c' // 0% 处的颜色
+                    },
+                    {
+                        offset: 1,
+                        color: '#e4fefc' // 100% 处的颜色
+                    }
+                ],
+                global: false // 缺省为 false
+            },
+            {
+                type: 'linear',
+                x: 0,
+                y: 0,
+                x2: 0,
+                y2: 1,
+                colorStops: [
+                    {
+                        offset: 0,
+                        color: '#55b1ff' // 0% 处的颜色
+                    },
+                    {
+                        offset: 1,
+                        color: '#e4fefc' // 100% 处的颜色
+                    }
+                ],
+                global: false // 缺省为 false
+            }
+        ]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+
+    //渲染标题列表
+    // const titleStr = arr.map((item, index) => `<button type="button" class="btn btn-xs ">${index + 1}</button>`).join('')
+    // document.querySelector('#btns').innerHTML = titleStr
+
+    let titleStr = ''
+    for (let key in arr) {
+        titleStr += `<button type="button" class="btn btn-xs ">${key}</button>`
+    }
+    document.querySelector('#btns').innerHTML = titleStr
+
+    // 排他思想 点击切换
+    const btnList = document.querySelectorAll('#btns button')
+    btnList.forEach(btn => {
+        btn.onclick = function () {
+            for (let i = 0; i < btnList.length; i++) {
+                btnList[i].classList.remove('btn-blue')
+            }
+            this.classList.add('btn-blue')
+
+            const index = this.innerHTML
+            // console.log(arr[index]);
+            // console.log(arr[index].map(item => item.hope_salary));
+            //echarts数据有变化，我们就可以通过setOption来重绘图表
+            myChart.setOption({
+                xAxis: {
+                    data: arr[index].map(item => item.name)
+                },
+                series: [
+                    {
+                        type: 'bar',
+                        data: arr[index].map(item => item.hope_salary),
+                    },
+                    {
+                        type: 'bar',
+                        data: arr[index].map(item => item.salary),
+                    }
+                ]
+            })
+        }
+    })
+    btnList[0].click()//模拟点击
+}
